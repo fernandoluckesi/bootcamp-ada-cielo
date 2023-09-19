91325f39c40df51649854d916976ae9b371365c2
Felipe Schiavini <felipe@stabel.com.br>
2023-09-19 17:23:40 -0300

    feat(rating): remove margin

--- src/components/StarRatingSelector/index.tsx
+++ src/components/StarRatingSelector/index.tsx
@@ -9,7 +9,7 @@ interface StarRatingSelectorProps {
 
 export const StarRatingSelector: React.FC<StarRatingSelectorProps> = ({rating, onClick}) => {
   return (
-    <Button variant="contained" color="secondary" onClick={onClick} sx={{borderRadius: 20, width: 140, marginTop: 20, marginLeft: 20}}>
+    <Button variant="contained" color="secondary" onClick={onClick} sx={{borderRadius: 20, width: 140}}>
         <Rating
           value={rating} size="medium" readOnly precision={1} emptyIcon={<StarIcon style={{ color: "grey.300" }} fontSize="inherit" />}
         />
