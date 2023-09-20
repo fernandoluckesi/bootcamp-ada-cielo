3dc13b47bcff62eb40b10ac8e22001e1a1d4dd6b
Felipe Schiavini <felipe@stabel.com.br>
2023-09-20 20:33:58 -0300

    feat(cart): add product to badge

--- src/utils/search.ts
+++ src/utils/search.ts
@@ -3,3 +3,5 @@ import { Product } from "../context/ProductsProvider.context";
 
 export const findProductById = (id:  Product['id'], cart: CartItem[]) => cart.find(item => item.product.id === id)
   
+export const capitalizeFirstLetter = (string: string) =>string.charAt(0).toUpperCase() + string.slice(1);
+
