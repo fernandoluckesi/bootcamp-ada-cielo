95575ab0ff88ef7075c9b2f28cb2948e72afda8a
Felipe Schiavini <felipe@stabel.com.br>
2023-09-20 21:57:07 -0300

    feat(pagination): create component

--- src/context/ProductsProvider.context.tsx
+++ src/context/ProductsProvider.context.tsx
@@ -16,10 +16,17 @@ interface ProductContextProps {
   filter: (input: Partial<Product>) => void;
   refreshData: () => void;
   changePage: (pageNumber: number) => Promise<void>;
+  loading: boolean;
+  paginationInfo: PaginationInfo
 }
 
 interface ProductsProviderProps {
   children: ReactNode;
+}
+
+interface PaginationInfo {
+  isNextPageAvailable: boolean;
+  currentPage: number; 
 }
 
 export const ProductsContext = createContext<ProductContextProps>({} as ProductContextProps);
@@ -27,20 +34,31 @@ export const ProductsContext = createContext<ProductContextProps>({} as ProductC
 export const ProductsProvider: React.FC<ProductsProviderProps> = ({
   children,
 }) => {
-
+  const [loading, setLoading] = useState<boolean>(false);
   const [productList, setProductList] = useState<Product[]>([])
   const [productFilteredList, setProductFilteredList] = useState<Product[]>([])
-
+  const [paginationInfo, setPaginationInfo] = useState<PaginationInfo>({currentPage: 1, isNextPageAvailable: true})
 
   const getData = async (pageNumber: number = 1, pageSize: number = 12) => {
+    setLoading(true)
     try {
       const response = await fetch(
-        `http://localhost:3000/products?pageSize=${pageSize}&pageNumber=${pageNumber}`
+        `http://localhost:3000/products?pageSize=${pageSize + 1}&pageNumber=${pageNumber}`
       );
       const products: Product[] = await response.json();
-      setProductList(products)
+
+      setPaginationInfo({
+        isNextPageAvailable: products.length > pageSize,
+        currentPage: pageNumber,
+      })
+
+      setProductList(products.slice(0, pageSize))
+
+     
     } catch (error) {
       console.error("Error: DataProvider.context.tsx:41 ~ getData:", error);
+    } finally {
+      setLoading(false)
     }
   };
 
@@ -56,7 +74,14 @@ export const ProductsProvider: React.FC<ProductsProviderProps> = ({
 
   return (
     <ProductsContext.Provider
-      value={{ refreshData: () => getData(), changePage: getData, filter, products: productFilteredList }}
+      value={{ 
+        refreshData: () => getData(), 
+        changePage: getData, 
+        filter, 
+        products: productFilteredList, 
+        loading,
+        paginationInfo
+      }}
     >
       {children}
     </ProductsContext.Provider>
