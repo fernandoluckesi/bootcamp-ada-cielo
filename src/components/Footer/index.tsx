e79f372f3ecc04d29cd4f956a2f627e273792c12
Carlos Matheus <51537701+cmdsouza@users.noreply.github.com>
2023-09-21 19:41:07 -0300

    feat: Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,25 +1,29 @@
-import { Box, Typography, Link } from '@mui/material';
+import { Box, Typography, Link, IconButton } from '@mui/material';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 const Footer = () => {
   return (
     <Box
-      bgcolor="primary.main" // Define a cor de fundo como azul (a cor primária do tema)
+      bgcolor="primary.main" // Define a cor de fundo como a cor primária do tema
       color="white" // Define a cor do texto como branco
       p={2} // Adiciona um espaçamento interno
-      textAlign="center" // Centraliza o conteúdo horizontalmente
+      display="flex" // Exibe os elementos em linha
+      alignItems="center" // Alinha os elementos verticalmente
+      justifyContent="space-between" // Espaçamento entre os elementos
       marginTop="2%"
     >
       <Typography variant="body2">
-        Nome do Projeto: Seu Projeto
+        © 2023 Cielo MarketPlace. Criado por Carlos, Fernando, Felipe, Lucas e Matheus.
       </Typography>
-      <Typography variant="body2">
-        Integrantes do Grupo: Carlos, Fernando, Felipe, Lucas e Matheus
-      </Typography>
-      <Typography variant="body2">
-        <Link href="https://github.com/fernandoluckesi/bootcamp-ada-cielo" target="_blank" rel="noopener noreferrer">
-          Github
-        </Link>
-      </Typography>
+      <Link
+        href="https://github.com/fernandoluckesi/bootcamp-ada-cielo"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <IconButton>
+          <GitHubIcon />
+        </IconButton>
+      </Link>
     </Box>
   );
 };
